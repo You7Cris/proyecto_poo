5ee0fe0bc054881794b0aa5f183c89475405558b
cristianmarint <31068007+cristianmarint@users.noreply.github.com>
2018-11-12 15:24:28 -0500

    recomendaciones inicio 2

--- Codigo/src/pantallas/inicio.java
+++ Codigo/src/pantallas/inicio.java
@@ -146,6 +146,7 @@ public class inicio {
 						
 						
 						//busca al usuario y si lo encuentra validacion es verdadera.
+						//resultSet es como un vector que comienza desde uno, en donde la primera posición es id_permiso etc.
 						ResultSet resultSet =statement.executeQuery("select id_permiso,id_usuario from usuario where user='"+user+"' AND pass='"+pass+"' ;");
 						if(resultSet.next() ==  true){validacion=true;};
 						ResultSetMetaData rsmd = (ResultSetMetaData) resultSet.getMetaData();
